bedf8a83a34c1d64f0e4d8bb83d40cbe68370fef
anui0115 <89232751+anui0115@users.noreply.github.com>
2022-03-19 22:00:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,2 @@
 import os
-<<<<<<< HEAD
 os.system('python function_20373873.py')
-
-
-<<<<<<< HEAD
-=======
-os.system('python function_20373898.py')
->>>>>>> exercise-20373898
-=======
-
->>>>>>> 880ced278b696b426434f00d884b6b9d4f6ba876
